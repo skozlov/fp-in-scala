a328490117de54c00b0b3c8a6e99c57e5307a1f0
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2019-12-07 21:45:37 +0700

    fold

--- src/main/scala/com/github/skozlov/fpinscala/Tree.scala
+++ src/main/scala/com/github/skozlov/fpinscala/Tree.scala
@@ -1,31 +1,58 @@
 package com.github.skozlov.fpinscala
 
-sealed trait Tree[+A]{
-	def size: Int
+import scala.annotation.tailrec
 
-	def depth: Int
+sealed trait Tree[+A]{
+	import Tree._
+
+	def size: Int = fold(0){(_, leftSize, rightSize) => 1 + leftSize + rightSize}
+
+	def depth: Int = fold(0){(_, leftDepth, rightDepth) => 1 + Math.max(leftDepth, rightDepth)}
+
+	def fold[B](seed: B)(f: (A, B, B) => B): B = {
+		sealed trait State
+
+		object State{
+			case class Start(tree: Tree[A]) extends State
+			case class LeftFolded(tree: Branch[A], leftResult: B) extends State
+			case class Folded(result: B) extends State
+		}
+
+		import State._
+
+		@tailrec
+		def fold(pathToRoot: List[State]): B = pathToRoot.head match {
+			case Start(Nil) => fold(Folded(seed) :: pathToRoot.tail)
+			case Start(Branch(_, left, _)) => fold(Start(left) :: pathToRoot)
+			case LeftFolded(Branch(_, _, right), _) => fold(Start(right) :: pathToRoot)
+			case Folded(result) =>
+				val parents = pathToRoot.tail
+				if (parents.isEmpty) {
+					result
+				} else {
+					(parents.head: @unchecked) match {
+						case Start(branch @ Branch(_, _, _)) => fold(LeftFolded(branch, result) :: parents.tail)
+						case LeftFolded(Branch(value, _, _), leftResult) =>
+							fold(Folded(f(value, leftResult, result)) :: parents.tail)
+					}
+				}
+		}
+
+		fold(List(Start(this)))
+	}
 }
 
 object Tree {
-	case object Nil extends Tree[Nothing] {
-		override def size: Int = 0
+	case object Nil extends Tree[Nothing]
 
-		override def depth: Int = 0
-	}
-
-	case class Branch[+A](value: A, left: Tree[A], right: Tree[A]) extends Tree[A] {
-		override def size: Int = left.size + right.size + 1
-
-		override def depth: Int = 1 + Math.max(left.depth, right.depth)
-	}
+	case class Branch[+A](value: A, left: Tree[A], right: Tree[A]) extends Tree[A]
 
 	def empty[A]: Tree[A] = Nil
 
 	def leaf[A](value: A): Branch[A] = Branch(value, Nil, Nil)
 
-	def max[A](tree: Tree[A])(implicit ordering: Ordering[A]): Option[A] = tree match {
-		case Nil => None
-		case Branch(value, left, right) => Some((value :: List(max(left), max(right)).flatten).max)
+	def max[A](tree: Tree[A])(implicit ordering: Ordering[A]): Option[A] = tree.fold(Option.empty[A]){
+		(value, maxLeft, maxRight) => Some((value :: List(maxLeft, maxRight).flatten).max)
 	}
 
 	def max[A](branch: Branch[A])(implicit ordering: Ordering[A]): A = max(branch.asInstanceOf[Tree[A]]).get
