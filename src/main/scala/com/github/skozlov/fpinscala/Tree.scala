31125db36ba4e0fdd4b7a30c147f32035aa2a219
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2019-12-07 19:33:26 +0700

    max

--- src/main/scala/com/github/skozlov/fpinscala/Tree.scala
+++ src/main/scala/com/github/skozlov/fpinscala/Tree.scala
@@ -13,5 +13,14 @@ object Tree {
 		override def size: Int = left.size + right.size + 1
 	}
 
+	def empty[A]: Tree[A] = Nil
+
 	def leaf[A](value: A): Branch[A] = Branch(value, Nil, Nil)
+
+	def max[A](tree: Tree[A])(implicit ordering: Ordering[A]): Option[A] = tree match {
+		case Nil => None
+		case Branch(value, left, right) => Some((value :: List(max(left), max(right)).flatten).max)
+	}
+
+	def max[A](branch: Branch[A])(implicit ordering: Ordering[A]): A = max(branch.asInstanceOf[Tree[A]]).get
 }
--- src/test/scala/com/github/skozlov/fpinscala/TreeSpec.scala
+++ src/test/scala/com/github/skozlov/fpinscala/TreeSpec.scala
@@ -9,4 +9,11 @@ class TreeSpec extends Spec {
 		Branch(0, leaf(1), leaf(2)).size shouldBe 3
 		Branch(0, leaf(1), Branch(2, leaf(3), Nil)).size shouldBe 4
 	}
+
+	"max" should "return max value" in {
+		max(Tree.empty[Int]) shouldBe None
+		max(leaf(0)) shouldBe 0
+		max(Branch(0, leaf(1), leaf(2))) shouldBe 2
+		max(Branch(2, leaf(0), leaf(1))) shouldBe 2
+	}
 }
